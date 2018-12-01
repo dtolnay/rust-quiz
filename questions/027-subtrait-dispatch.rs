cbeec912f48c89d9f40a3fde29800946ac3f2e3d
David Tolnay <dtolnay@gmail.com>
2018-11-30 17:46:21 -0800

    Focus on how BothTraits case differs from C++

--- questions/027-subtrait-dispatch.rs
+++ questions/027-subtrait-dispatch.rs
@@ -1,36 +1,28 @@
-
 trait Base {
-    fn method(&self) {print!("1")}
+    fn method(&self) {
+        print!("1");
+    }
 }
 
 trait Derived: Base {
-    fn method(&self) {print!("2")}
+    fn method(&self) {
+        print!("2");
+    }
 }
-
-struct OnlyBase;
-
-impl Base for OnlyBase {
-    fn method(&self) {print!("3")}
-}
-
 
 struct BothTraits;
 impl Base for BothTraits {}
 impl Derived for BothTraits {}
 
-// dynamic dispatch
-fn dynamic(x: &dyn Base) {
-    x.method()
+fn dynamic_dispatch(x: &dyn Base) {
+    x.method();
 }
 
-// static dispatch
-fn stat<T: Base>(x: &T) {
+fn static_dispatch<T: Base>(x: T) {
     x.method();
 }
 
 fn main() {
-    dynamic(&OnlyBase);
-    stat(&OnlyBase);
-    dynamic(&BothTraits);
-    stat(&BothTraits);
+    dynamic_dispatch(&BothTraits);
+    static_dispatch(BothTraits);
 }
